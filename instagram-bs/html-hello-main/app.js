00933b6c3b71a70109ae65332b4606d6662258b1
SOMBRA99 <lyonelworks99@gmail.com>
2025-08-01 12:55:11 +0200

    cambios sugeridos

--- instagram-bs/html-hello-main/app.js
+++ instagram-bs/html-hello-main/app.js
@@ -5,26 +5,27 @@ const content = document.getElementById("content")
 btnGrid.addEventListener("click",function(){
     for(const item of content.children){
         item.classList.remove("col-12")
-        item.classList.add("col-4")
+        item.classList.add("col-lg-4","col-md-6", "col-sm-12")
+         
+        const card = item.querySelector(".card")
+        card.classList.remove("w-50")
+        card.classList.add("w-100")
         
-        for(const itemTag of item.children){
-            itemTag.classList.remove("w-50")
-            itemTag.classList.add("w-100")
-        }
+        const elemtsToHide = card.querySelectorAll(".card-header, .card-body")
+        elemtsToHide.forEach(element => element.style.display="none") 
     }
-}
-)
+})
 
 btnList.addEventListener("click",function(){
     for(const item of content.children){
-        console.log(content.children)
-        item.classList.remove("col-4")
+        item.classList.remove("col-lg-4","col-md-6", "col-sm-12")
         item.classList.add("col-12")
+    
+        const card = item.querySelector(".card")
+        card.classList.remove("w-100")
+        card.classList.add("w-50")
         
-        for(const itemTag of item.children){
-            itemTag.classList.remove("w-100")
-            itemTag.classList.add("w-50")
-        }
+        const elementsToShow = card.querySelectorAll(".card-header, .card-body")
+        elementsToShow.forEach(element => element.style.display="")
     }
-}
-)
+})
